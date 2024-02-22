7794aa4e8a73aa31e1b1d2f6baf1c5b7bd8a3a4d
alanent <33031877+alanent@users.noreply.github.com>
2024-02-22 10:42:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from uvicorn import run
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__=="__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
